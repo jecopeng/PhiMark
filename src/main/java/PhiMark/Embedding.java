cea1e8df135623ba6050d69ad821a72395ff24dc
1 <877045168@qq.com>
2025-06-04 20:05:41 +0800

    1

---
+++ src/main/java/PhiMark/Embedding.java
@@ -0,0 +1,15 @@
+package PhiMark;
+
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+public class Embedding {
+    public static void main(String[] args) throws CsvValidationException, SQLException, IOException, NoSuchAlgorithmException {
+        watermark wm = new watermark();
+        wm.encode("queaterlywages2000");//for QCEW dataset.
+        //wm.encode("geography");
+    }
+}
