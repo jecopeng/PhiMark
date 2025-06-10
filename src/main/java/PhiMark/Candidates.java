e4bc40d5a53922760b2a386920b9882312e8409d
1 <877045168@qq.com>
2025-06-10 22:43:25 +0800

    1

---
+++ src/main/java/PhiMark/Candidates.java
@@ -0,0 +1,12 @@
+package PhiMark;
+
+import net.didion.jwnl.data.Word;
+
+public class Candidates {
+    int pos;
+    Word[] words;
+    Candidates(int pos, Word[] words){
+        this.pos = pos;
+        this.words = words;
+    }
+}
---
+++ src/main/java/PhiMark/Wordandtags.java
@@ -0,0 +1,12 @@
+package PhiMark;
+
+public class Wordandtags {
+    String word;
+    String tag;
+    int pos;//位置
+    public Wordandtags(String word, String tag, int pos){
+        this.word = word;
+        this.tag = tag;
+        this.pos = pos;
+    }
+}
